245d42bcd4f4e48a13f4d3ba38099ddc832959c7
Mehmet Konukcu <mehmetext68@gmail.com>
2022-11-14 10:29:59 +0300

    dosya taşındı

--- algoritma-1/yildiz_tablo/yildiz_tablo.cpp
+++ algoritma-1/yildiz_tablo/yildiz_tablo.cpp
@@ -4,10 +4,11 @@ int main() {
 	
 	int giris = 20;
 	
-	printf("Sayi gir: "); scanf("%d");
+	printf("Sayi gir: "); scanf("%d", &giris);
 	
 	printf("\n\n");
 	
+	printf("1. tablo: \n");
 	for (int i = 1; i <= giris; i++) {
 		for (int j = 0; j < i; j++) printf("*");
 		printf("\n");
@@ -20,6 +21,8 @@ int main() {
 	//2. 1 - 2
 	//3. 0 - 3
 	
+	
+	printf("2. tablo: \n");
 	for (int i = 1; i <= giris; i++) {
 		for (int j = 0; j < giris - i; j++) printf(" ");
 		for (int j = 0; j < i; j++) printf("*");
@@ -33,6 +36,7 @@ int main() {
 	//2. 1 - 3 - 1
 	//3. 0 - 5 - 0
 	
+	printf("3. tablo: \n");
 	for (int i = 1; i <= giris; i++) {
 		for (int j = 0; j < giris - i; j++) printf(" ");
 		for (int j = 0; j < i * 2 - 1; j++) printf("*");
@@ -63,6 +67,7 @@ int main() {
 	//2. 1 - 2 - 1
 	//3. 0 - 4 - 0
 	
+	printf("4. tablo: \n");
 	for (int i = 1; i <= giris; i++) {
 		for (int j = 0; j < giris-i; j++) printf(" ");
 		printf("/");
@@ -74,9 +79,9 @@ int main() {
 	}
 	
 	//giris: 3
-	//1. 0 - 4 - 0 3*2 - 2*1 => 2 * (3-1)
-	//2. 1 - 2 - 1 3*2 - 2*2 => 2 * (3-2)
-	//3. 2 - 0 - 2 3*2 - 2*3 => 2 * (3-3)
+	//1. 0 - 4 - 0 -- 3*2 - 2*1 => 2 * (3-1)
+	//2. 1 - 2 - 1 -- 3*2 - 2*2 => 2 * (3-2)
+	//3. 2 - 0 - 2 -- 3*2 - 2*3 => 2 * (3-3)
 	
 	for (int i = 1; i <= giris; i++) {
 		for (int j = 0; j < i-1; j++) printf(" ");
@@ -84,6 +89,36 @@ int main() {
 		for (int j = 0; j < 2 * (giris - i); j++) printf("*");
 		printf("/");
 		for (int j = 0; j < i-1; j++) printf(" ");
+		
+		printf("\n");
+	}
+	
+	printf("\n\n");
+	
+	/*
+	
+	    *    
+	   * *   
+	  *   *  
+	 *     * 
+	*       *
+	
+	*/
+	
+	//giris: 5
+	//1. 4 - 0 - 4
+	//2. 3 - 1 - 3
+	//3. 2 - 3 - 2
+	//4. 1 - 5 - 1
+	//5. 0 - 7 - 0
+	
+	printf("5. tablo: \n");
+	for (int i = 1; i <= giris; i++) {
+		for (int j = 0; j < giris - i; j++) printf(" ");
+		printf("*");
+		for (int j = 0; j < 1 + (i-2)*2; j++) printf(" ");
+		if (i != 1) printf("*");
+		
 		
 		printf("\n");
 	}
