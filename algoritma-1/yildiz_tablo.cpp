c2eb344adca023365321440d9b5525060337d9de
Mehmet Konukcu <mehmetext68@gmail.com>
2022-11-14 10:27:35 +0300

    içi boş yıldız kodu eklendi

--- algoritma-1/yildiz_tablo.cpp
+++ algoritma-1/yildiz_tablo.cpp
@@ -79,9 +79,9 @@ int main() {
 	}
 	
 	//giris: 3
-	//1. 0 - 4 - 0 3*2 - 2*1 => 2 * (3-1)
-	//2. 1 - 2 - 1 3*2 - 2*2 => 2 * (3-2)
-	//3. 2 - 0 - 2 3*2 - 2*3 => 2 * (3-3)
+	//1. 0 - 4 - 0 -- 3*2 - 2*1 => 2 * (3-1)
+	//2. 1 - 2 - 1 -- 3*2 - 2*2 => 2 * (3-2)
+	//3. 2 - 0 - 2 -- 3*2 - 2*3 => 2 * (3-3)
 	
 	for (int i = 1; i <= giris; i++) {
 		for (int j = 0; j < i-1; j++) printf(" ");
@@ -89,6 +89,36 @@ int main() {
 		for (int j = 0; j < 2 * (giris - i); j++) printf("*");
 		printf("/");
 		for (int j = 0; j < i-1; j++) printf(" ");
+		
+		printf("\n");
+	}
+	
+	printf("\n\n");
+	
+	/*
+	
+	    *    
+	   * *   
+	  *   *  
+	 *     * 
+	*       *
+	
+	*/
+	
+	//giris: 5
+	//1. 4 - 0 - 4
+	//2. 3 - 1 - 3
+	//3. 2 - 3 - 2
+	//4. 1 - 5 - 1
+	//5. 0 - 7 - 0
+	
+	printf("5. tablo: \n");
+	for (int i = 1; i <= giris; i++) {
+		for (int j = 0; j < giris - i; j++) printf(" ");
+		printf("*");
+		for (int j = 0; j < 1 + (i-2)*2; j++) printf(" ");
+		if (i != 1) printf("*");
+		
 		
 		printf("\n");
 	}
